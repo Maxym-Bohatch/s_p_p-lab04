6c2901d14929a39eb161f1b5cb0eb217969b256e
bohac <bohac@RYZEN5500U>
2024-04-23 17:31:09 +0300

    Добавьте файлы проекта.

---
+++ s_p_p lab04/s_p_p lab04.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Базовий клас "Велосипед"
+class Bicycle {
+protected:
+    string model;
+    int wheelSize;
+
+public:
+    Bicycle(const string& _model, int _wheelSize) : model(_model), wheelSize(_wheelSize) {}
+
+    void displayInfo() const {
+        cout << "Model: " << model << endl;
+        cout << "Wheel Size: " << wheelSize << " inches" << endl;
+    }
+};
+
+// Похідний клас "Заїзд"
+class Ride : public Bicycle {
+private:
+    int number;
+    string route;
+    float time;
+
+public:
+    Ride(const string& _model, int _wheelSize, int _number, const string& _route, float _time)
+        : Bicycle(_model, _wheelSize), number(_number), route(_route), time(_time) {}
+
+    void displayInfo() const {
+        Bicycle::displayInfo();
+        cout << "Number: " << number << endl;
+        cout << "Route: " << route << endl;
+        cout << "Time: " << time << " hours" << endl;
+    }
+};
+
+int main() {
+    // Створення об'єкту класу "Заїзд"
+    Ride ride("Mountain Bike", 26, 12345, "Mountain Trail", 2.5);
+
+    // Вивід інформації про заїзд
+    cout << "Ride Details:" << endl;
+    ride.displayInfo();
+
+    return 0;
+}
